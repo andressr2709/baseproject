ec15a782bada8770d28072855a45a1fc7222c044
Andres Sancho <andres76579@gmail.com>
2023-03-31 02:22:56 +0000

    Initial config export

--- project/web/sites/default/settings.ddev.php
+++ project/web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'URldpDoEYDOAePEmnJoMnawKrVkFsLIhfKiyRlypqCkcMFJIvrMaOmFGxgFEGVha';
+$settings['hash_salt'] = 'mySJVhjLQVDkhDRKyeacMHqfDbtBDlHQOlyTsAorCziDQbDJsfaMMazgWAqtpXnh';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
